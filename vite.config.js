92bf376b26e41c4fc6219df19af62c45f3dd1de4
Dylan Milavec <dylanmilavec@Dylans-MacBook-Pro.local>
2025-02-03 21:52:56 -0500

    Simplified Vite configuration and moved index.html to root

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression()
-  ],
-  root: path.resolve(__dirname, ''),
+  plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-      input: path.resolve(__dirname, 'index.html')
-    },
-    minify: 'terser',
-    cssCodeSplit: true,
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true
+    outDir: 'dist'
   }
 })
