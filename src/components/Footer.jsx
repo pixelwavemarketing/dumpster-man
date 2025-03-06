15118b76be12312a6c0c66922ca37fd2045f9a6b
Dylan Milavec <dylanmilavec@Dylans-MacBook-Pro.local>
2025-03-06 10:21:28 -0500

    Added Yelp social icon and centered social media links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faYelp } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -19,14 +20,24 @@ function Footer() {
           <div className="contact-info">
             <p><a href="tel:+15189202618">(518) 920-2618</a></p>
             <p><a href="mailto:thedumpsterman01@gmail.com">thedumpsterman01@gmail.com</a></p>
-            <div className="social-media">
+            <div className="social-media" style={{ display: 'flex', gap: '1rem', alignItems: 'center', justifyContent: 'center', marginTop: '0.5rem' }}>
               <a 
                 href="https://www.facebook.com/profile.php?id=61558249876337" 
                 target="_blank" 
                 rel="noopener noreferrer" 
                 className="social-icon facebook-icon"
+                style={{ fontSize: '1.5rem' }}
               >
                 <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a 
+                href="https://www.yelp.com/biz/the-dumpster-man-mechanicville?osq=the+dumpster+man&override_cta=Get+pricing" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="social-icon yelp-icon"
+                style={{ fontSize: '1.5rem' }}
+              >
+                <FontAwesomeIcon icon={faYelp} />
               </a>
             </div>
           </div>
